6213b293c9f3fd5feea638c64aa41309a1f0f9dd
samleenz <samleenz@me.com>
2022-04-11 16:42:50 +1000

    make slurm submission script

---
+++ slurm_dps.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# slurm_dps.sh
+# sam lee
+# 2022-04-11
+# -------EDIT HERE------
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lee.sa@wehi.edu.au 
+#SBATCH -J pdmr_dps
+#SBATCH -t 24:00:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+# ----------------------
+
+# do some things
+module load R
+
+Rscript R/download_paired_samples.R
